4b047095ef6cb8e5661cd118354bea517171ba92
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2022-05-21 13:31:26 +0900

    스파크도커파일 추가

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -9,7 +9,3 @@ if [ "$SPARK_MODE" = "worker" ]; then
     /opt/spark/sbin/start-worker.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT}
 fi
 
-if [ "$SPARK_MODE" = "zeppelin" ]; then
-    cd /opt/zeppelin
-    /bin/bash ./bin/zeppelin.sh
-fi
